fb0548cf1cecdc95de0f0a75e9dd491a1677d003
Peer Sahab <peersahab@Peers-MacBook-Pro.local>
2024-01-30 14:40:25 -0800

    Initial setup

---
+++ core/src/com/bullethell/game/BulletHellGame.java
@@ -0,0 +1,78 @@
+package com.bullethell.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class BulletHellGame extends Game implements InputProcessor {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+
+	@Override
+	public boolean keyDown(int i) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int i) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char c) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int i, int i1, int i2, int i3) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int i, int i1, int i2, int i3) {
+		return false;
+	}
+
+	@Override
+	public boolean touchCancelled(int i, int i1, int i2, int i3) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int i, int i1, int i2) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int i, int i1) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(float v, float v1) {
+		return false;
+	}
+}
