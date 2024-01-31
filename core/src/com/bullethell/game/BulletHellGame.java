81349f8041002dc1968b34e92268d133cacebf46
jnie <jingjing.nie@wsu.edu>
2024-01-31 12:13:48 -0800

    add enemies_a

--- core/src/com/bullethell/game/BulletHellGame.java
+++ core/src/com/bullethell/game/BulletHellGame.java
@@ -3,22 +3,39 @@ package com.bullethell.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.bullethell.game.screens.PlayScreen;
-
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class BulletHellGame extends Game {
 	public SpriteBatch batch;
-
+	private Texture texture;
+	private enemies enemy_a;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		texture = new Texture("type A.png");
+		TextureRegion region = new TextureRegion(texture);
+		enemy_a = new enemies(region);
+		enemy_a.setPosition(100, 100);
 		setScreen(new PlayScreen(this));
 	}
 
 	@Override
 	public void render () {
-		super.render();
+		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		batch.begin();
+		enemy_a.draw(batch);
+		batch.end();
+		//super.render();
 	}
 	
 	@Override
-	public void dispose () {}
+	public void dispose () {
+		batch.dispose();
+		texture.dispose();
+	}
 
 }
---
+++ core/src/com/bullethell/game/enemies.java
@@ -0,0 +1,9 @@
+package com.bullethell.game;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class enemies extends Sprite {
+    public enemies(TextureRegion region) {
+        super(region);
+    }
+}
