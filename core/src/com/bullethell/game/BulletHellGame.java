aad986973dbd8163711ce4f5e009e85bad35d659
Sheheryar <spirzada@eecs.wsu.edu>
2024-02-06 20:25:33 -0800

    Json settings setup + Code enhancements

--- core/src/com/bullethell/game/BulletHellGame.java
+++ core/src/com/bullethell/game/BulletHellGame.java
@@ -5,52 +5,82 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.bullethell.game.settings.GlobalSettings;
+import com.bullethell.game.settings.Settings;
+import com.bullethell.game.utils.JsonUtil;
+
+import java.util.Objects;
 
 public class BulletHellGame extends Game implements InputProcessor {
 	SpriteBatch batch;
 	Sprite sprite;
 	Texture player;
+	Texture backgroundTexture;
 	boolean movingLeft = false;
 	boolean movingRight = false;
 	boolean movingDown = false;
 	boolean movingUp = false;
 	boolean slowSpeed = false;
 
+	public static Settings settings;
+
 	@Override
 	public void create () {
+		preflight();
 		batch = new SpriteBatch();
+		backgroundTexture = new Texture("space_background.png");
 		player = new Texture("player.png");
 		sprite = new Sprite(player);
 		sprite.setPosition(Gdx.graphics.getWidth()/2 - sprite.getWidth()/2,
 				Gdx.graphics.getHeight()/2 - sprite.getHeight()/2);
 		Gdx.input.setInputProcessor(this);
+
 	}
 
 	@Override
 	public void render () {
-		if(slowSpeed) {
-			if (movingLeft)
-				sprite.translateX(-1f);
-			if (movingRight)
-				sprite.translateX(1f);
-			if (movingUp)
-				sprite.translateY(1f);
-			if (movingDown)
-				sprite.translateY(-1f);
-		}else{
-			if (movingLeft)
-				sprite.translateX(-5f);
-			if (movingRight)
-				sprite.translateX(5f);
-			if (movingUp)
-				sprite.translateY(5f);
-			if (movingDown)
-				sprite.translateY(-5f);
+		GlobalSettings gs = settings.getGlobalSettings();
+		float speedFactor = slowSpeed ? gs.getSlowSpeed() : gs.getNormalSpeed();
+
+		//defining boundaries which prevents the player going out from the frame
+		float currPositionX = sprite.getX();
+		float currPositionY = sprite.getY();
+
+		if (movingLeft) {
+			currPositionX -= speedFactor;
 		}
+		if (movingRight) {
+			currPositionX += speedFactor;
+		}
+		if (movingUp) {
+			currPositionY += speedFactor;
+		}
+		if (movingDown) {
+			currPositionY -= speedFactor;
+		}
+
+		//checking for positions on X axis
+		if(currPositionX < 0){
+			currPositionX = 0; //left edge case
+		}else if(currPositionX + sprite.getWidth() > Gdx.graphics.getWidth()){
+			currPositionX = Gdx.graphics.getWidth() - sprite.getWidth(); //right edge case
+		}
+
+		//checking for positions for Y axis
+		if(currPositionY < 0){
+			currPositionY = 0;//bottom edge case
+		}
+		else if(currPositionY + sprite.getHeight() > Gdx.graphics.getHeight()){
+			currPositionY = Gdx.graphics.getHeight() - sprite.getHeight(); //top edge case
+		}
+
+		sprite.setPosition(currPositionX, currPositionY);
 
 		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
+		batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.draw(sprite, sprite.getX(), sprite.getY());
+		sprite.draw(batch);
 		batch.end();
 	}
 	
@@ -58,19 +88,23 @@ public class BulletHellGame extends Game implements InputProcessor {
 	public void dispose () {
 		batch.dispose();
 		player.dispose();
+		backgroundTexture.dispose();
 	}
 
 	@Override
 	public boolean keyDown(int keycode) {
-		if(keycode == Input.Keys.LEFT)
+		String key = Input.Keys.toString(keycode);
+		GlobalSettings gs = settings.getGlobalSettings();
+
+		if(Objects.equals(key, gs.getMoveLeft()))
 			movingLeft = true;
-		if(keycode == Input.Keys.RIGHT)
+		if(Objects.equals(key, gs.getMoveRight()))
 			movingRight = true;
-		if(keycode == Input.Keys.UP)
+		if(Objects.equals(key, gs.getMoveUp()))
 			movingUp = true;
-		if(keycode == Input.Keys.DOWN)
+		if(Objects.equals(key, gs.getMoveDown()))
 			movingDown = true;
-		if(keycode == Input.Keys.SPACE)
+		if(Objects.equals(key, gs.getSlowMode()))
 			slowSpeed = true;
 
 		return false;
@@ -78,15 +112,18 @@ public class BulletHellGame extends Game implements InputProcessor {
 
 	@Override
 	public boolean keyUp(int keycode) {
-		if(keycode == Input.Keys.LEFT)
+		String key = Input.Keys.toString(keycode);
+		GlobalSettings gs = settings.getGlobalSettings();
+
+		if(Objects.equals(key, gs.getMoveLeft()))
 			movingLeft = false;
-		if(keycode == Input.Keys.RIGHT)
+		if(Objects.equals(key, gs.getMoveRight()))
 			movingRight = false;
-		if(keycode == Input.Keys.UP)
+		if(Objects.equals(key, gs.getMoveUp()))
 			movingUp = false;
-		if(keycode == Input.Keys.DOWN)
+		if(Objects.equals(key, gs.getMoveDown()))
 			movingDown = false;
-		if(keycode == Input.Keys.SPACE)
+		if(Objects.equals(key, gs.getSlowMode()))
 			slowSpeed = false;
 		return false;
 	}
@@ -124,5 +161,10 @@ public class BulletHellGame extends Game implements InputProcessor {
 	@Override
 	public boolean scrolled(float v, float v1) {
 		return false;
+	}
+
+	private void preflight() {
+		JsonUtil jsonUtil = new JsonUtil();
+		settings = jsonUtil.deserializeJson("settings/settings.json", Settings.class);
 	}
 }
