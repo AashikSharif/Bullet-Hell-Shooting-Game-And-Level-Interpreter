d897e7178bf6df1aaf10a0026079c0a25f6d365d
Peer Sahab <peersahab@Peers-MBP.lan>
2024-02-12 17:43:22 -0800

    Adds Enemy classes and GameSystem + code cleanup

--- core/src/com/bullethell/game/entities/Entity.java
+++ core/src/com/bullethell/game/entities/Entity.java
@@ -1,29 +1,22 @@
 package com.bullethell.game.entities;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.bullethell.game.systems.AssetHandler;
 
 public abstract class Entity {
     protected Vector2 position;
     public Sprite sprite;
 
-    public Entity (float x, float y, String texturePath) {
+    public Entity (float x, float y, String entity, AssetHandler assetHandler) {
         this.position = new Vector2(x, y);
-        Texture texture = new Texture(Gdx.files.internal(texturePath));
-        this.sprite = new Sprite(texture);
+        this.sprite = new Sprite(assetHandler.getAssetTexture(entity));
         this.sprite.setPosition(x, y);
     }
 
     public void update () {
         this.sprite.setPosition(position.x, position.y);
     }
-
-//    public void render (SpriteBatch spriteBatch) {
-//        sprite.draw(spriteBatch);
-//    }
 
     public Vector2 getPosition() {
         return position;
--- core/src/com/bullethell/game/systems/GameSystem.java
+++ core/src/com/bullethell/game/systems/GameSystem.java
@@ -1,4 +1,72 @@
 package com.bullethell.game.systems;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.bullethell.game.controllers.PlayerController;
+import com.bullethell.game.entities.Enemy;
+import com.bullethell.game.entities.Entity;
+import com.bullethell.game.entities.Player;
+import com.bullethell.game.settings.Settings;
+import com.bullethell.game.utils.JsonUtil;
+
+import java.util.List;
+
 public class GameSystem {
+    private float time;
+
+    private Settings settings;
+
+    private AssetHandler assetHandler = new AssetHandler();
+
+    private Texture background;
+
+    private Player player;
+
+    private PlayerController playerController;
+
+    private List<Enemy> enemyList;
+
+    public GameSystem() {
+        init();
+    }
+
+    public void init() {
+        this.time = 0;
+        loadSettings();
+        assetHandler.load(settings.getAssets());
+
+        player = new Player(100, 100, assetHandler);
+        playerController = new PlayerController(player, settings.getPlayerSettings());
+
+        background = assetHandler.getAssetTexture("background");
+    }
+
+    public void update(float time) {
+        this.time += time;
+        playerController.listen();
+    }
+
+    public void render(SpriteBatch spriteBatch, float time) {
+        // combined renders
+        update(time);
+        renderBackground(spriteBatch);
+        renderEntity(spriteBatch, player);
+    }
+
+    private void renderEntity(SpriteBatch spriteBatch, Entity entity) {
+        spriteBatch.draw(entity.sprite, entity.sprite.getX(), entity.sprite.getY());
+    }
+
+    private void renderBackground(SpriteBatch spriteBatch) {
+        spriteBatch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+    }
+
+    private void renderEnemies() {
+    }
+
+    private void loadSettings() {
+        JsonUtil jsonUtil = new JsonUtil();
+        settings = jsonUtil.deserializeJson("settings/settings.json", Settings.class);
+    }
 }
